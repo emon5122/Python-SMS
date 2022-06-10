992dbe257466148b7da6fc34a0ad2679f478f11f
Istiak Hassan Emon <emon5122@gmail.com>
2022-06-10 20:15:39 +0600

    We can send messages now

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from ipaddress import IPv4Address
 from pyairmore.request import AirmoreSession
 from pyairmore.services.device import DeviceService
+from pyairmore.services.messaging import MessagingService
 from decouple import config
 
 D_IP=config("IP") #Reading IP from .env
@@ -8,3 +9,10 @@ D_IP=config("IP") #Reading IP from .env
 ip = IPv4Address(f'{D_IP}')  # whatever server's address is
 session = AirmoreSession(ip)  # port is default to 2333
 service = DeviceService(session) #starts a session
+Mservice = MessagingService(session)
+
+if session.request_authorization():
+    Mservice.send_message("Number", "testing")
+
+else:
+    print('Nah')
